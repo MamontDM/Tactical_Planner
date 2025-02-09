666995168adcf181bc83c9dcc597095c21a1f72b
MamontDm <mamonovDY@gmail.com>
2025-02-09 13:06:52 +0200

    another small fix front  env var

--- planner-app/front/src/components/LeftSidebar/ComponentList/PlannerHelper/components/ShipList/shipList.jsx
+++ planner-app/front/src/components/LeftSidebar/ComponentList/PlannerHelper/components/ShipList/shipList.jsx
@@ -7,7 +7,7 @@ import { useShipContext } from '../../../../../../hooks/useShipContext';
 const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : process.env.REACT_APP_API_BASE_URL;
+    : import.meta.env.VITE_API_BASE_URL;
 
 const ShipList = ({uniqueKey, type, index, clearListFlag}) =>{
   const { filteredShips, addShip, ships, removeShip } = useShipContext();
